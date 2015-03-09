6ac4a84790c7f2a405f7d70caeaea404f0eff656
patshiu <pat@gqpbj.com>
2015-03-09 19:31:13 -0400

    Whatever it works.

--- R3_test/ascii.js
+++ R3_test/ascii.js
@@ -1,31 +1,18 @@
 
 function ASCIIF (idNum, w, h, totalFrames, spriteURL) {
 	this.id = idNum;
-	this.monospcW = this.findMonoWidth(); 
 	this.url = spriteURL; 
 	this.h = -1; //If no valid height input, the h = -1
 	this.h = h;  
 	this.w = w; 
 	this.spriteW = 0; 
-	this.frames = totalFrames; 
+	this.tFrames = totalFrames; 
+	this.cFrame = 1; //Current frame
 /*	this.rgbData = []; //2D array: Array of Array of rgb data of each frame
 	this.toneData = []; //2D array: Array of Array of tonal data of each frame*/
 	this.asciiData = ""; //Array of ascii characters that make up the picture
 }
 
-ASCIIF.prototype.findMonoWidth = function() {
-/*	var tempChar = document.createElement("div");
-	var tempSpan = tempChar.appendChild(document.createElement("span"));
-	tempSpan.appendChild(document.createTextNode("0"));
-	tempSpan.id = "testWidth";
-	tempChar.id = "testWidthDiv";
-	document.getElementById("container").appendChild(tempChar);
-	var w = document.getElementById("testWidth").width;
-	document.getElementById("container").removeChild(tempChar);
-	return w; */
-
-	return 6.45;
-}
 
 
 ASCIIF.prototype.computeASCIIF = function() {
@@ -33,7 +20,9 @@ ASCIIF.prototype.computeASCIIF = function() {
 	//Create a image of sprite on the page, hide it. 
 	var tempSprite = document.createElement("img");
 	tempSprite.src = this.url; 
-	tempSprite.style.display = "none";
+	console.log(tempSprite.src);
+	console.log("tempSprite width = " + tempSprite.width);
+	//tempSprite.style.display = "none";
 
 	//Create canvas element
 	var tempCanvas = document.createElement("canvas");
@@ -48,7 +37,7 @@ ASCIIF.prototype.computeASCIIF = function() {
 	tc.drawImage(tempSprite, 0, 0, tempSprite.width, this.h);
 
 	//Access pixel data
-	var pixels = tc.getImageData(0, 0, tempSprite.width, this.h );
+	var pixels = tc.getImageData(0, 0, tempCanvas.width, this.h );
 
 
 	//every pixel gives 4 integers -> r, g, b, a
@@ -81,31 +70,50 @@ ASCIIF.prototype.computeASCIIF = function() {
 }
 
 ASCIIF.prototype.loadASCIIF = function() {
-	var aniCanvas = document.createElement("pre");
-	aniCanvas.id = this.id; 
-	aniCanvas.className = "ascii";
-	aniCanvas.style.width = this.w+"ex"; 
+	var aniCanvas = document.createElement("div");
+	aniCanvas.className = "asciiBox";
+	var asciifSprite = aniCanvas.appendChild(document.createElement("pre"));
+	asciifSprite.id = this.id; 
+	asciifSprite.className = "ascii";
+	aniCanvas.style.width = this.w*charWidth+"px"; 
+	console.log(this.w + " x " + charWidth + " = " + aniCanvas.style.width);
 	aniCanvas.style.marginBottom = 10+"em"; 
-	aniCanvas.style.marginLeft = 0+"px"; 
+	aniCanvas.style.marginLeft = 0+"px";
 
 	//Load in ascii 
 	for (var i = 0; i < this.h; i++){
-		aniCanvas.appendChild(document.createTextNode(this.asciiData.slice(i*this.spriteW, this.spriteW*(i+1))));
-		aniCanvas.appendChild(document.createElement("br"));
+		asciifSprite.appendChild(document.createTextNode(this.asciiData.slice(i*this.spriteW, this.spriteW*(i+1))));
+		asciifSprite.appendChild(document.createElement("br"));
 	};
 
 	document.getElementById("container").appendChild(aniCanvas);
 }
 
 ASCIIF.prototype.animateASCIIF = function() {
-	var aniCanvas = document.getElementById(this.id);
+	console.log(this.cFrame);
+
+	//Check to see if page zoom / font-size has changed (jQuery)
+	charWidth = $("#testWidth > span").width()/161;
+
+	var asciifSprite = document.getElementById(this.id);
+
+	//Check if last frame is reached, if yes, set current frame back to 0. 
+	if (this.cFrame >= this.tFrames - 1 ) {
+		this.cFrame = 0; 
+	}
+	asciifSprite.style.left = Math.ceil(-1 * this.cFrame * $("#testWidth > span").width()) + "px"; 
+	this.cFrame++; 
+	console.log(this.id + " is currently at frame " + this.cFrame);
 }
 
 var testImgs = []; 
+var charWidth;
 
 window.onload = function(){
 	
-	
+	charWidth = $("#testWidth > span").width()/161;
+	console.log("Char Width is " + charWidth);
+
 	testImgs[0] = new ASCIIF(01, 161, 120, 22, "images/8805-sm.png" );	
 	testImgs[1] = new ASCIIF(02, 161, 120, 22, "images/8805-sm.png" );	
 
@@ -115,5 +123,16 @@ window.onload = function(){
 	testImgs[1].computeASCIIF();
 	testImgs[1].loadASCIIF();
 
+	setInterval( loop , 100);
+	function loop() {
+		testImgs[0].animateASCIIF();
+		testImgs[0].animateASCIIF();
+	}
 
 }
+
+/* jQuery */
+$(document).ready(function(){
+	console.log("wh0 l3t th3 pr0n out");
+	console.log($("#testWidth > span").width());
+});
